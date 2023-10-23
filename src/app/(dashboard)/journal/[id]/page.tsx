a8dd1c1252ca6b0e51bfc6a58a486228885bb461
Patrik Pyrzanowski <patrik.pyrzanowski@gmail.com>
2023-10-24 00:28:47 +0200

    Add styles to Analysis summary.

--- src/app/(dashboard)/journal/[id]/page.tsx
+++ src/app/(dashboard)/journal/[id]/page.tsx
@@ -13,14 +13,42 @@ const getEntry = async (id: string) => {
   const entry = await prisma.journalEntry.findUniqueOrThrow({
     where: { userId_id: { userId: user.id, id } },
   });
+
   return entry;
 };
 
 const EntryPage: FunctionComponent<Props> = async ({ params }) => {
   const entry = await getEntry(params.id);
+  const analysisData = [
+    { name: "Summary", value: "Good" },
+    { name: "Subject", value: "First Entry" },
+    { name: "Mood", value: "Happy" },
+    { name: "Negative", value: "False" },
+  ];
+
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry} />
+    <div className="h-full w-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+                key={item.name}
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
